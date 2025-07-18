ff8905201998e34efbe81e6ab71460e646b2a92b
AshimChoudhary <choudharyashim99@gmail.com>
2025-07-18 22:30:17 +0530

    Implemented Hero Section

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" alt="logo-svg" className="scale-90" />
+      <img src="/images/menu.svg" alt="menu-svg" className="w-10" />
+    </nav>
+  );
+};
+export default Navbar;
